2e74738c2528f362688b8184a6f835278ed23417
Yale Hartmann <yale.hartmann@uni-bremen.de>
2024-09-23 16:05:39 +0200

    some more updates

--- src/livenodes_matplotlib/draw_gmm.py
+++ src/livenodes_matplotlib/draw_gmm.py
@@ -5,10 +5,10 @@ import matplotlib as mpl
 from .draw_scatter import Draw_scatter
 
 from livenodes import Ports_collection
-from livenodes_common_ports import Port_Dict, Ports_empty, Port_Data, Port_List_Str
+from livenodes_common_ports import Port_Dict, Ports_empty, Port_BTS_Number, Port_List_Str
 
 class Ports_in(Ports_collection):
-    data: Port_Data = Port_Data("Data")
+    data: Port_BTS_Number = Port_BTS_Number("Data")
     channels: Port_List_Str = Port_List_Str("Channel Names")
     meta: Port_Dict = Port_Dict("HMM Meta")
     states: Port_Dict = Port_Dict("Hypo States")
